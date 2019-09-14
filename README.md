265e2b9f567be2dea9fb962910a2d68d1cf016bc
kg86 <kg86dev@gmail.com>
2019-09-14 14:59:01 +0900

    add simple demo

--- README.md
+++ README.md
@@ -4,6 +4,44 @@ See [the demo page](https://kg86.github.io/visstr/dist/vis_str.html).
 
 Documentation is [here](https://kg86.github.io/visstr/docs/index.html)
 
-## TODO
+## Usage
 
-Write usage.
+Put a canvas in your html.
+Import `vis_str.js` like the following.
+```html
+<canvas id="canvas" style="border:1px solid #000000;"> </canvas>
+<script type="text/javascript" src="./vis_str.js"></script>
+```
+
+Create an visstr object in your js.
+```js
+const visstr = window.visstr
+
+const canvas = document.querySelector('#canvas')
+// input string
+const s = 'abaababaabaab'
+// create visstr object.
+const vstr = new visstr.VisStr(canvas, s)
+```
+
+Create an range list you want to draw, and add line style and color, and then groups them so that ehy are not overlap with each others.
+```js
+// create occurrences of aba.
+const occ_aba = [[0, 2], [3, 5], [5, 7], [8, 10]]
+// add line style and color.
+const ranges = vstr.makeRanges(occ_aba, 'arrow', '#ff0000')
+// make group so that they are not overlap with each others.
+const range_groups = vstr.nonOverlapRangeIdxs(ranges)
+```
+
+Draw the range list by `vstr.draw`.
+```js
+// draw the occurrences.
+vstr.draw(s, range_groups)
+```
+
+The canvas draws the range list as following.
+
+![](others/occ_aba.png)
+
+See [the demo](https://kg86.github.io/visstr/dist/vis_str_demo_occ.html).
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,35 @@
 const path = require('path')
 const TypedocWebpackPlugin = require('typedoc-webpack-plugin')
 
-module.exports = {
+const configDemo = {
+  mode: 'development',
+  // devtool: 'source-map',
+  // devtool: 'cheap-eval-source-map',
+  // devtool: 'cheap-module-eval-source-map',
+  devtool: 'inline-source-map',
+  entry: {
+    vis_str_demo: './src/vis_str_demo.ts',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    modules: ['node_modules', path.resolve(__dirname, 'src')],
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
+        // TypeScript をコンパイルする
+        use: 'ts-loader',
+      },
+    ],
+  },
+}
+const configLibrary = {
   mode: 'development',
   // devtool: 'source-map',
   // devtool: 'cheap-eval-source-map',
@@ -9,11 +37,13 @@ module.exports = {
   devtool: 'inline-source-map',
   entry: {
     vis_str: './src/vis_str.ts',
-    vis_str_demo: './src/vis_str_demo.ts',
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '_bundles'),
+    library: 'visstr',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
   },
   resolve: {
     modules: ['node_modules', 'color-convert', path.resolve(__dirname, 'src')],
@@ -44,3 +74,6 @@ module.exports = {
     ),
   ],
 }
+
+module.exports = [configDemo, configLibrary]
+// module.exports = [configLibrary]
